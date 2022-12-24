4b9b416607184c35745c4966942136d315b73afc
Luke Hodkinson <furious.luke@gmail.com>
2022-12-24 12:57:15 +1100

    :tada: First commit

---
+++ clack_auth/plugin.py
@@ -0,0 +1,38 @@
+import click
+from clack import ClackApp
+
+from .auth import build_token, resolve_public_key_by_fingerprint
+from .errors import RegistrationError
+
+
+def plugin(app: ClackApp):
+    @app.main.command()
+    @click.argument("email")
+    def register(email):
+        app.api_request("register", "post", {"email": email}, error_class=RegistrationError)
+
+    @app.main.group()
+    def public_keys():
+        pass
+
+    @public_keys.command("add")
+    @click.option("--email", envvar=f"{app.envname}_EMAIL")
+    @click.argument("fingerprint")
+    def add_public_key(email, fingerprint):
+        token = build_token(email)
+        public_key = resolve_public_key_by_fingerprint(fingerprint)
+        r = app.api_request(
+            "publickeys",
+            "post",
+            {"public_key": public_key},
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        print(r.json())
+
+
+    @public_keys.command("list")
+    @click.option("--email", envvar=f"{app.envname}_EMAIL")
+    def list_public_keys(email):
+        token = build_token(email)
+        r = app.api_request("publickeys", "get", headers={"Authorization": f"Bearer {token}"})
+        print(r.json())
